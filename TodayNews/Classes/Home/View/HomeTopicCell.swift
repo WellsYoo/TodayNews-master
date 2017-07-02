2a0f697935d0ff10107e6db674431bdff3035f34
hrscy <15399070815@163.com>
2017-07-02 21:41:55 +0800

    0701 修改首页

---
+++ TodayNews/Classes/Home/View/HomeTopicCell.swift
@@ -0,0 +1,29 @@
+//
+//  HomeTopicCell.swift
+//  TodayNews
+//
+//  Created by 杨蒙 on 2017/7/2.
+//  Copyright © 2017年 hrscy. All rights reserved.
+//
+
+import UIKit
+
+class HomeTopicCell: UITableViewCell {
+
+    var weitoutiao: WeiTouTiao? {
+        didSet {
+            
+        }
+    }
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+        
+    }
+    
+}
