89180436ac30de636c210aa248acb88eb36d8dd1
hrscy <15399070815@163.com>
2018-02-06 21:21:26 +0800

    0206 新增截图

---
+++ News/News/Classes/RedPackage/View/RedPackageHeaderView.swift
@@ -0,0 +1,21 @@
+//
+//  RedPackageHeaderView.swift
+//  News
+//
+//  Created by 杨蒙 on 2018/2/6.
+//  Copyright © 2018年 hrscy. All rights reserved.
+//
+
+import UIKit
+
+class RedPackageHeaderView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
