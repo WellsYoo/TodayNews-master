fd1239cfcb78b2afde9580c2c22457319aeee5d0
ywh <8632638933@qq.com>
2018-10-29 22:00:35 +0800

    【Resource】图片资源提交

---
+++ WellsNews/WellsNews/Classes/Main/Controller/WNTabBarController.swift
@@ -0,0 +1,24 @@
+//
+//  WNTabBarController.swift
+//  WellsNews
+//
+//  Created by Wells on 2018/10/29.
+//  Copyright © 2018 Wells. All rights reserved.
+//
+
+import UIKit
+
+class WNTabBarController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    //MARK:- Private
+    private func addChildViewControllers() {
+        
+    }
+
+}
---
+++ WellsNews/WellsNews/Classes/ViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  WellsNews
+//
+//  Created by Wells on 2018/10/14.
+//  Copyright © 2018 Wells. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+}
